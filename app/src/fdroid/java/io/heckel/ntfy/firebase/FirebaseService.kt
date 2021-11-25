b9e86252c9964b30e7f8b7d96dd99481f15ad2c5
Philipp Heckel <pheckel@datto.com>
2021-11-24 22:16:36 -0500

    Make FirebaseService an actual Service

--- app/src/fdroid/java/io/heckel/ntfy/firebase/FirebaseService.kt
+++ app/src/fdroid/java/io/heckel/ntfy/firebase/FirebaseService.kt
@@ -1,5 +1,12 @@
 package io.heckel.ntfy.firebase
 
-class FirebaseService {
-    // Dummy to keep F-Droid flavor happy
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+// Dummy to keep F-Droid flavor happy
+class FirebaseService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
 }
