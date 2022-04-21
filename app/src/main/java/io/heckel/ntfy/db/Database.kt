4ff9a37052aec1eaf4365127ed2ad1b5d17d8ac4
Philipp Heckel <pheckel@datto.com>
2022-04-21 19:55:13 -0400

    Fix migration

--- app/src/main/java/io/heckel/ntfy/db/Database.kt
+++ app/src/main/java/io/heckel/ntfy/db/Database.kt
@@ -241,7 +241,7 @@ abstract class Database : RoomDatabase() {
 
         private val MIGRATION_9_10 = object : Migration(9, 10) {
             override fun migrate(db: SupportSQLiteDatabase) {
-                db.execSQL("ALTER TABLE Notification ADD COLUMN actions TEXT NOT NULL DEFAULT('')")
+                db.execSQL("ALTER TABLE Notification ADD COLUMN actions TEXT")
             }
         }
     }
