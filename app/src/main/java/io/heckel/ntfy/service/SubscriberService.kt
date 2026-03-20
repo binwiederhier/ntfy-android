0da0ea95c67b2e087057b59a4649a1a9056fa7cf
Martin Erpicum <martin@erpicum.net>
2026-03-20 05:51:40 +0100

    fix: ensure network validation is checked for internet capability in SubscriberService

--- app/src/main/java/io/heckel/ntfy/service/SubscriberService.kt
+++ app/src/main/java/io/heckel/ntfy/service/SubscriberService.kt
@@ -345,6 +345,7 @@ class SubscriberService : Service() {
         val network = connectivityManager.activeNetwork ?: return false
         val capabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
         return capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            && capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
     }
 
     private fun maybeAutoDismissConnectionAlert() {
